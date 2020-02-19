7444f0d818e25d487faa86be3b9eafeda7bc2226
Aishwarya Sharma <aishwaryasharma8961@gmail.com>
2020-02-19 13:01:55 -0600

    cypress installed and working

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,15 @@
+describe('My First Test', function() {
+    it('Does not do much!', function() {
+      expect(true).to.equal(true)
+    })
+  })
+  describe('My First Test', function() {
+    it('Does not do much!', function() {
+      expect(true).to.equal(false)
+    })
+  })
+  describe('My First Test', function() {
+    it('Visits the Kitchen Sink', function() {
+      cy.visit('https://example.cypress.io')
+    })
+  })
