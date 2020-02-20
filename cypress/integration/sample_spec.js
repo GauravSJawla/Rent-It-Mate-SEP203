d4e779823e3d1b94cdcdf1b65b541f746f8d46f1
Aishwarya Sharma <aishwaryasharma8961@gmail.com>
2020-02-19 19:50:22 -0600

    travis added

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,13 +1,4 @@
-describe('My First Test', function() {
-    it('Does not do much!', function() {
-      expect(true).to.equal(true)
-    })
-  })
-  describe('My First Test', function() {
-    it('Does not do much!', function() {
-      expect(true).to.equal(false)
-    })
-  })
+
   describe('My First Test', function() {
     it('Visits the Kitchen Sink', function() {
       cy.visit('https://example.cypress.io')
