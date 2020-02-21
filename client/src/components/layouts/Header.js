8cecd2f3eae3c5edb4a5b45f7c79540a2a4b2b3e
Aishwarya Sharma <aishwaryasharma8961@gmail.com>
2020-02-20 19:00:19 -0600

    changes in header file

--- client/src/components/layouts/Header.js
+++ client/src/components/layouts/Header.js
@@ -14,6 +14,14 @@ import AccountCircle from '@material-ui/icons/AccountCircle';
 import MailIcon from '@material-ui/icons/Mail';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import MoreIcon from '@material-ui/icons/MoreVert';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 
 const useStyles = makeStyles(theme => ({
   grow: {
@@ -113,7 +121,7 @@ export default function PrimarySearchAppBar() {
       open={isMenuOpen}
       onClose={handleMenuClose}
     >
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose} to="/login">Login</MenuItem>
       <MenuItem onClick={handleMenuClose}>My account</MenuItem>
     </Menu>
   );
