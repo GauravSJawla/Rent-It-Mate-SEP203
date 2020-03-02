d5000a82a4201ff437023d65f69d4dfda85de42c
Aishwarya Sharma <aishwaryasharma8961@gmail.com>
2020-03-01 20:07:43 -0600

    added basic third party auth

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
-
+var passport = require('passport');
 const app = express();
 
 // Connect Database
@@ -19,6 +19,11 @@ app.use('/api/profile', require('./routes/api/profile'));
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
+
+  //using for third party authentication
+  var passport = require("passport");
+  var app = express();
+
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
