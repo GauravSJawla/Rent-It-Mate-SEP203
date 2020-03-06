49a4b60a1e4fe898167a60fcdd4d1f69e9453086
Aishwarya Sharma <aishwaryasharma8961@gmail.com>
2020-03-06 11:26:39 -0600

    deleted files

--- cypress/integration/register.spec.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-import Register from '../../client/src/components/auth/Register';
-
-describe("Register Test", ()) => {
-    it('can get the form', () => {
-        cy.visit('http://localhost:3000');
-    })
-}
