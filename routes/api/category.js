4f7eb80235b5884eaf9af62c1f3b25cf8c54e431
GauravSJawla <gauravsinghjawla@gmail.com>
2020-03-12 13:14:04 -0500

    Little change left

--- routes/api/category.js
+++ routes/api/category.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-//const auth = require('../../middleware/auth');
+const auth = require('../../middleware/auth');
 const { validationResult } = require('express-validator/check');
 
 const Category = require('../../models/Category');
