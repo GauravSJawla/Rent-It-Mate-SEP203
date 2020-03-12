97c4dc420df40d6349471063b4fa61ab153849a1
SELT171920 <hemalatha-ramachandran@uiowa.edu>
2020-03-11 20:37:10 -0500

    Added protected route for dashboard and Updateprofile

--- client/src/views/ProfilePage/UpdateProfile.js
+++ client/src/views/ProfilePage/UpdateProfile.js
@@ -1,46 +1,226 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import propTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 // @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
-import InputLabel from "@material-ui/core/InputLabel";
+import { makeStyles } from '@material-ui/core/styles';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Icon from '@material-ui/core/Icon';
+// @material-ui/icons
+import Email from '@material-ui/icons/Email';
+import PeopleIcon from '@material-ui/icons/People';
+import PersonIcon from '@material-ui/icons/Person';
 // core components
-import GridItem from "components/Grid/GridItem.js";
-import GridContainer from "components/Grid/GridContainer.js";
-import CustomInput from "components/CustomInput/CustomInput.js";
-import Button from "components/CustomButtons/Button.js";
-import Card from "components/Card/Card.js";
-import CardHeader from "components/Card/CardHeader.js";
-import CardAvatar from "components/Card/CardAvatar.js";
-import CardBody from "components/Card/CardBody.js";
-import CardFooter from "components/Card/CardFooter.js";
+import Footer from 'components/Footer/Footer.js';
+import GridContainer from 'components/Grid/GridContainer.js';
+import GridItem from 'components/Grid/GridItem.js';
+import Button from 'components/CustomButtons/Button.js';
+import Card from 'components/Card/Card.js';
+import CardBody from 'components/Card/CardBody.js';
+import CardHeader from 'components/Card/CardHeader.js';
+import CardFooter from 'components/Card/CardFooter.js';
+import CustomInput from 'components/CustomInput/CustomInput.js';
+import styles from 'assets/jss/material-kit-react/views/loginPage.js';
 
-const styles = {
-    cardCategoryWhite: {
-      color: "rgba(255,255,255,.62)",
-      margin: "0",
-      fontSize: "14px",
-      marginTop: "0",
-      marginBottom: "0"
-    },
-    cardTitleWhite: {
-      color: "#FFFFFF",
-      marginTop: "0px",
-      minHeight: "auto",
-      fontWeight: "300",
-      fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
-      marginBottom: "3px",
-      textDecoration: "none"
-    }
-  };
-  
+import image from 'assets/img/bg7.jpg';
 
+//Import register from other component
+import { register } from '../../actions/auth';
 
 const useStyles = makeStyles(styles);
 
-export default function UpdateProfile(){
-    const classes = useStyles();
-    return(
-        <div>
-             <h4 className={classes.cardTitleWhite}>Edit Profile</h4>
+function UpdateProfile() {
+  const [cardAnimaton, setCardAnimation] = React.useState('cardHidden');
+  setTimeout(function() {
+    setCardAnimation('');
+  }, 700);
+
+//   const [formData, setFormData] = useState({
+//     name: '',
+//     username: '',
+//     email: '',
+//     password: '',
+//     password2: ''
+//   });
+ const classes = useStyles();
+
+//   //OnChange event Handler
+//   const onChange = e =>
+//     setFormData({
+//       ...formData,
+//       [e.target.id]: e.target.value
+//     });
+
+//   const { name, username, email, password, password2 } = formData;
+
+//   // OnSubmit Event Handler
+//   const onSubmit = e => {
+//     e.preventDefault();
+//     console.log('name in else:' + { name });
+//     if (password !== password2) {
+//       alert('Passwords do not match');
+//     } else {
+//       register({ name, username, email, password });
+//     }
+//   };
+
+//   if (isAuthenticated) {
+//     return <Redirect to='/emailVerifyPage' />;
+//   }
+  return (
+    <div>
+      <div
+        className={classes.pageHeader}
+        style={{
+          backgroundImage: 'url(' + image + ')',
+          backgroundSize: 'cover',
+          backgroundPosition: 'top center'
+        }}
+      >
+        <div className={classes.container}>
+          <GridContainer justify='center'>
+            <GridItem xs={12} sm={12} md={4}>
+              <Card className={classes[cardAnimaton]}>
+                <form className={classes.form}>
+                  <CardHeader color='primary' className={classes.cardHeader}>
+                    <h4>Edit Profile</h4>
+                    <div className={classes.socialLine}>
+                      <Button
+                        justIcon
+                        href='#pablo'
+                        target='_blank'
+                        color='transparent'
+                        //onClick={e => e.preventDefault()}
+                      >
+                        <i className={'fab fa-google'} />
+                      </Button>
+                    </div>
+                  </CardHeader>
+                  {/* <CardBody>
+                    <CustomInput
+                      labelText='Full Name...'
+                      id='name'
+                      formControlProps={{
+                        fullWidth: true
+                      }}
+                      inputProps={{
+                        type: 'text',
+                        value: name,
+                        required : true,
+                        onChange: e => onChange(e),
+                        endAdornment: (
+                          <InputAdornment position='end'>
+                            <PeopleIcon className={classes.inputIconsColor} />
+                          </InputAdornment>
+                        )
+                      }}
+                    />
+                    <CustomInput
+                      labelText='User Name...'
+                      id='username'
+                      formControlProps={{
+                        fullWidth: true
+                      }}
+                      inputProps={{
+                        type: 'username',
+                        value: username,
+                        required: true,
+                        onChange: e => onChange(e),
+                        endAdornment: (
+                          <InputAdornment position='end'>
+                            <PersonIcon className={classes.inputIconsColor} />
+                          </InputAdornment>
+                        )
+                      }}
+                    />
+                    <CustomInput
+                      labelText='Email...'
+                      id='email'
+                      formControlProps={{
+                        fullWidth: true
+                      }}
+                      inputProps={{
+                        type: 'email',
+                        value: email,
+                        required : true,
+                        onChange: e => onChange(e),
+                        endAdornment: (
+                          <InputAdornment position='end'>
+                            <Email className={classes.inputIconsColor} />
+                          </InputAdornment>
+                        )
+                      }}
+                    />
+                    <CustomInput
+                      labelText='Password'
+                      id='password'
+                      formControlProps={{
+                        fullWidth: true
+                      }}
+                      inputProps={{
+                        type: 'password',
+                        value: password,
+                        required: true,
+                        onChange: e => onChange(e),
+                        endAdornment: (
+                          <InputAdornment position='end'>
+                            <Icon className={classes.inputIconsColor}>
+                              lock_outline
+                            </Icon>
+                          </InputAdornment>
+                        ),
+                        autoComplete: 'off'
+                      }}
+                    />
+                    <CustomInput
+                      labelText='Confirm Password'
+                      id='password2'
+                      formControlProps={{
+                        fullWidth: true
+                      }}
+                      inputProps={{
+                        type: 'password',
+                        value: password2,
+                        required : true,
+                        onChange: e => onChange(e),
+                        endAdornment: (
+                          <InputAdornment position='end'>
+                            <Icon className={classes.inputIconsColor}>
+                              lock_outline
+                            </Icon>
+                          </InputAdornment>
+                        ),
+                        autoComplete: 'off'
+                      }}
+                    />
+                  </CardBody>
+                  <CardFooter className={classes.cardFooter}>
+                    <Button simple type='submit' color='primary' size='lg'>
+                      Get started
+                    </Button>
+                  </CardFooter> */}
+                </form>
+              </Card>
+            </GridItem>
+          </GridContainer>
         </div>
-    )
+        <Footer whiteFont />
+      </div>
+    </div>
+  );
 }
+
+// RegisterPage.propTypes = {
+//   register: propTypes.func.isRequired,
+//   isAuthenticated: propTypes.bool
+// };
+
+// const mapStateToProps = state => ({
+//   isAuthenticated: state.auth.isAuthenticated
+// });
+
+// export default connect(
+//   mapStateToProps,
+//   { register }
+// )(RegisterPage);
+export default UpdateProfile;
