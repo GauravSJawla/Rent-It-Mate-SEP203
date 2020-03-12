6f18b70aaa31130e0264a45958557da082a590d9
GauravSJawla <gauravsinghjawla@gmail.com>
2020-03-11 20:49:21 -0500

    Half working

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -6,8 +6,6 @@ const bcrypt = require('bcryptjs');
 const auth = require('../../middleware/auth');
 const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator/check');
-const config = require('config');
-const { check , validationResult} = require('express-validator');
 
 const User = require('../../models/Users');
 
