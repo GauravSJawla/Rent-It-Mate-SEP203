42fae6f66c9f0e691a802cf0722bb259f1ddfd65
SELT171920 <hemalatha-ramachandran@uiowa.edu>
2020-03-12 17:02:10 -0500

    API change in profile.js to retrieve user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const profile = require('../../models/Profile');
+const user = require('../../models/Users');
 
-// @route   GET api/profile
-// @desc    Test route
-// @access  Public
-router.get('/', (req, res) => res.send('Profile route'));
+// @route GET api/profile/me
+// @desc get current user profile
+// @access Private
 
+router.get('/me', auth, async(req,res) => {
+    try{
+        const userProfile = await profile.findOne({user: req.user.id}).populate(
+            'user',['name','email']
+        );
+        if(!userProfile){
+            return res.status(400).json({msg: 'You are yet to create your profile'});
+        }
+        res.json(userProfile);
+
+    }
+    catch(err){
+        console.err(err.message);
+        res.status(500).send('server error');  
+    }  
+})
+
+
+// @route Post api/profile
+// @desc Add it to profile database
+// @access private
 module.exports = router;
