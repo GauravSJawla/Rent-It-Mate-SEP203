9055b9c7388f580723eb89a6982dec54e8126de8
SELT171920 <hemalatha-ramachandran@uiowa.edu>
2020-03-24 17:43:35 -0500

    Initial cypress code for update-profile

--- cypress/integration/create-profile.spec.js
+++ cypress/integration/create-profile.spec.js
@@ -1,8 +1,31 @@
 /// <reference types="cypress" />
-describe('login page', () => {
+describe('create-profile page', () => {
     // beforeEach(() => {
     //     cy.visit('http://localhost:3000/login')
     // })
+    
+    // before(() => {
+    //     cy.request('POST','/api/auth', 
+    //             {username : 'Minnie', password: 'minnie'}).
+    //             then((response) => {
+    //                 const token = response.body.token;
+    //                 expect(response.body).to.have(token)
+    //                 cy.request('DELETE', '/api/profile', {
+    //                             'auth' : {
+    //                                 'x-auth-token' : token
+    //                              }
+    //     })
+    // })
+    //     cy.request('POST','/api/users', {
+    //         name : 'Minnie',
+    //       username : 'Minnie',
+    //       email : 'minnie@gmail.com',
+    //       password : 'minnie'
+    //     }).then((response) => {
+    //         expect(response.status).to.be(200)
+    //     })
+    // })
+
     it('dashboard has create profile link if there is no profile for the user', () => {
         cy.visit('http://localhost:3000/login')
         cy.get('input[id="username"]').type('Minnie')
@@ -13,20 +36,7 @@ describe('login page', () => {
         cy.url({timeout:5000}).should('includes','/create-profile')
     })
 
-    it('creates-profile for a new user', () => {
-        cy.get('input[id="address1"]').type('688,Collins road')
-        cy.get('input[id="city"]').type('Iowa City')
-        cy.get('input[id="state"]').type('Iowa')
-        cy.get('input[id="country"]').type('USA')
-        cy.get('input[id="zipcode"]').type(87675)
-        cy.get('input[id="homePhone"]').type(1234567890)
-        cy.get('button[type="submit').click()
-        cy.url({timeout : 5000}).should('includes','/dashboard')
-        cy.contains('has')
-    })
-
     it('stays on same page if address field is not filled', () => {
-        cy.get('input[id="address1"]').type('')
         cy.get('input[id="city"]').type('Iowa City')
         cy.get('input[id="state"]').type('Iowa')
         cy.get('input[id="country"]').type('USA')
@@ -37,48 +47,90 @@ describe('login page', () => {
     })
 
     it('stays on same page if city field is not filled', () => {
-        cy.get('input[id="address1"]').type('688,Collins road')
-        cy.get('input[id="city"]').type('')
-        cy.get('input[id="state"]').type('Iowa')
-        cy.get('input[id="country"]').type('USA')
-        cy.get('input[id="zipcode"]').type(87675)
-        cy.get('input[id="homePhone"]').type(1234567890)
+        cy.get('input[id="address1"]').clear().type('688,Collins road')
+        cy.get('input[id="city"]').clear()
+        cy.get('input[id="state"]').clear().type('Iowa')
+        cy.get('input[id="country"]').clear().type('USA')
+        cy.get('input[id="zipcode"]').clear().type(87675)
+        cy.get('input[id="homePhone"]').clear().type(1234567890)
         cy.get('button[type="submit').click()
         cy.url({timeout : 5000}).should('includes','/create-profile')
     })
 
     it('stays on same page if state field is not filled', () => {
-        cy.get('input[id="address1"]').type('688,Collins road')
-        cy.get('input[id="city"]').type('Iowa City')
-        cy.get('input[id="state"]').type('')
-        cy.get('input[id="country"]').type('USA')
-        cy.get('input[id="zipcode"]').type(87675)
-        cy.get('input[id="homePhone"]').type(1234567890)
+        cy.get('input[id="address1"]').clear().type('688,Collins road')
+        cy.get('input[id="city"]').clear().type('Iowa City')
+        cy.get('input[id="state"]').clear()
+        cy.get('input[id="country"]').clear().type('USA')
+        cy.get('input[id="zipcode"]').clear().type(87675)
+        cy.get('input[id="homePhone"]').clear().type(1234567890)
         cy.get('button[type="submit').click()
         cy.url({timeout : 5000}).should('includes','/create-profile')
     })
 
     it('stays on same page if country field is not filled', () => {
-        cy.get('input[id="address1"]').type('688,Collins road')
-        cy.get('input[id="city"]').type('Iowa City')
-        cy.get('input[id="state"]').type('Iowa')
-        cy.get('input[id="country"]').type('')
-        cy.get('input[id="zipcode"]').type(87675)
-        cy.get('input[id="homePhone"]').type(1234567890)
+        cy.get('input[id="address1"]').clear().type('688,Collins road')
+        cy.get('input[id="city"]').clear().type('Iowa City')
+        cy.get('input[id="state"]').clear().type('Iowa')
+        cy.get('input[id="country"]').clear()
+        cy.get('input[id="zipcode"]').clear().type(87675)
+        cy.get('input[id="homePhone"]').clear().type(1234567890)
         cy.get('button[type="submit').click()
         cy.url({timeout : 5000}).should('includes','/create-profile')
     })
 
     it('stays on same page if zipcode field is not filled', () => {
-        cy.get('input[id="address1"]').type('688,Collins road')
-        cy.get('input[id="city"]').type('Iowa City')
-        cy.get('input[id="state"]').type('Iowa')
-        cy.get('input[id="country"]').type('USA')
-        cy.get('input[id="zipcode"]').type()
-        cy.get('input[id="homePhone"]').type(1234567890)
+        cy.get('input[id="address1"]').clear().type('688,Collins road')
+        cy.get('input[id="city"]').clear().type('Iowa City')
+        cy.get('input[id="state"]').clear().type('Iowa')
+        cy.get('input[id="country"]').clear().type('USA')
+        cy.get('input[id="zipcode"]').clear()
+        cy.get('input[id="homePhone"]').clear().type(1234567890)
         cy.get('button[type="submit').click()
         cy.url({timeout : 5000}).should('includes','/create-profile')
     })
+
+    it('stays on same page if Homephone field is not filled', () => {
+        cy.get('input[id="address1"]').clear().type('688,Collins road')
+        cy.get('input[id="city"]').clear().type('Iowa City')
+        cy.get('input[id="state"]').clear().type('Iowa')
+        cy.get('input[id="country"]').clear().type('USA')
+        cy.get('input[id="zipcode"]').clear().type(87675)
+        cy.get('input[id="homePhone"]').clear()
+        cy.get('button[type="submit').click()
+        cy.url({timeout : 5000}).should('includes','/create-profile')
+    })
+
+    it('creates-profile for a new user', () => {
+        cy.get('input[id="address1"]').clear().type('688,Collins road')
+        cy.get('input[id="city"]').clear().type('Iowa City')
+        cy.get('input[id="state"]').clear().type('Iowa')
+        cy.get('input[id="country"]').clear().type('USA')
+        cy.get('input[id="zipcode"]').clear().type(87675)
+        cy.get('input[id="homePhone"]').clear().type(1234567890)
+        cy.get('button[type="submit').click()
+        cy.url({timeout : 5000}).should('includes','/dashboard')
+        cy.contains('has')
+    })
+
+    it('should not have create-profile if profile is created for the user', () => {
+        cy.visit('http://localhost:3000/login')
+        cy.get('input[id="username"]').type('Minnie')
+        cy.get('input[id="password"]').type('minnie')
+        cy.get('button[type = "submit"]').click()
+        cy.url({timeout : 5000}).should('includes','/dashboard')
+        cy.contains('has')
+    })
+
+    it('redirects to update-profile page', () => {
+        cy.contains('Profile').click().then(() => {
+            cy.get('a[href="/edit-profile"]').click()
+            cy.url({timeout:5000}).should('includes','/edit-profile')
+        })
+        
+    })
+
+    
     
 
     
---
+++ cypress/integration/update-profile.spec.js
@@ -0,0 +1,123 @@
+/// <reference types="cypress" />
+describe('create-profile page', () => {
+    // beforeEach(() => {
+    //     cy.visit('http://localhost:3000/login')
+    // })
+    
+    // before(() => {
+    //     cy.request('POST','/api/auth', 
+    //             {username : 'Minnie', password: 'minnie'}).
+    //             then((response) => {
+    //                 const token = response.body.token;
+    //                 expect(response.body).to.have(token)
+    //                 cy.request('DELETE', '/api/profile', {
+    //                             'auth' : {
+    //                                 'x-auth-token' : token
+    //                              }
+    //     })
+    // })
+    //     cy.request('POST','/api/users', {
+    //         name : 'Minnie',
+    //       username : 'Minnie',
+    //       email : 'minnie@gmail.com',
+    //       password : 'minnie'
+    //     }).then((response) => {
+    //         expect(response.status).to.be(200)
+    //     })
+    // })
+
+    it('dashboard do not have create profile link if profile is already available', () => {
+        cy.visit('http://localhost:3000/login')
+        cy.get('input[id="username"]').type('Minnie')
+        cy.get('input[id="password"]').type('minnie')
+        cy.get('button[type = "submit"]').click()
+        cy.url({timeout : 5000}).should('includes','/dashboard')
+        cy.contains('Profile').click().then(() => {
+            cy.get('a[href="/edit-profile"]').click()
+            cy.url({timeout:5000}).should('includes','/update-profile')
+        })
+        
+    })
+
+    // it('stays on same page if address field is not filled', () => {
+    //     cy.get('input[id="city"]').type('Iowa City')
+    //     cy.get('input[id="state"]').type('Iowa')
+    //     cy.get('input[id="country"]').type('USA')
+    //     cy.get('input[id="zipcode"]').type(87675)
+    //     cy.get('input[id="homePhone"]').type(1234567890)
+    //     cy.get('button[type="submit').click()
+    //     cy.url({timeout : 5000}).should('includes','/create-profile')
+    // })
+
+    // it('stays on same page if city field is not filled', () => {
+    //     cy.get('input[id="address1"]').clear().type('688,Collins road')
+    //     cy.get('input[id="city"]').clear()
+    //     cy.get('input[id="state"]').clear().type('Iowa')
+    //     cy.get('input[id="country"]').clear().type('USA')
+    //     cy.get('input[id="zipcode"]').clear().type(87675)
+    //     cy.get('input[id="homePhone"]').clear().type(1234567890)
+    //     cy.get('button[type="submit').click()
+    //     cy.url({timeout : 5000}).should('includes','/create-profile')
+    // })
+
+    // it('stays on same page if state field is not filled', () => {
+    //     cy.get('input[id="address1"]').clear().type('688,Collins road')
+    //     cy.get('input[id="city"]').clear().type('Iowa City')
+    //     cy.get('input[id="state"]').clear()
+    //     cy.get('input[id="country"]').clear().type('USA')
+    //     cy.get('input[id="zipcode"]').clear().type(87675)
+    //     cy.get('input[id="homePhone"]').clear().type(1234567890)
+    //     cy.get('button[type="submit').click()
+    //     cy.url({timeout : 5000}).should('includes','/create-profile')
+    // })
+
+    // it('stays on same page if country field is not filled', () => {
+    //     cy.get('input[id="address1"]').clear().type('688,Collins road')
+    //     cy.get('input[id="city"]').clear().type('Iowa City')
+    //     cy.get('input[id="state"]').clear().type('Iowa')
+    //     cy.get('input[id="country"]').clear()
+    //     cy.get('input[id="zipcode"]').clear().type(87675)
+    //     cy.get('input[id="homePhone"]').clear().type(1234567890)
+    //     cy.get('button[type="submit').click()
+    //     cy.url({timeout : 5000}).should('includes','/create-profile')
+    // })
+
+    // it('stays on same page if zipcode field is not filled', () => {
+    //     cy.get('input[id="address1"]').clear().type('688,Collins road')
+    //     cy.get('input[id="city"]').clear().type('Iowa City')
+    //     cy.get('input[id="state"]').clear().type('Iowa')
+    //     cy.get('input[id="country"]').clear().type('USA')
+    //     cy.get('input[id="zipcode"]').clear()
+    //     cy.get('input[id="homePhone"]').clear().type(1234567890)
+    //     cy.get('button[type="submit').click()
+    //     cy.url({timeout : 5000}).should('includes','/create-profile')
+    // })
+
+    // it('stays on same page if Homephone field is not filled', () => {
+    //     cy.get('input[id="address1"]').clear().type('688,Collins road')
+    //     cy.get('input[id="city"]').clear().type('Iowa City')
+    //     cy.get('input[id="state"]').clear().type('Iowa')
+    //     cy.get('input[id="country"]').clear().type('USA')
+    //     cy.get('input[id="zipcode"]').clear().type(87675)
+    //     cy.get('input[id="homePhone"]').clear()
+    //     cy.get('button[type="submit').click()
+    //     cy.url({timeout : 5000}).should('includes','/create-profile')
+    // })
+
+    // it('creates-profile for a new user', () => {
+    //     cy.get('input[id="address1"]').clear().type('688,Collins road')
+    //     cy.get('input[id="city"]').clear().type('Iowa City')
+    //     cy.get('input[id="state"]').clear().type('Iowa')
+    //     cy.get('input[id="country"]').clear().type('USA')
+    //     cy.get('input[id="zipcode"]').clear().type(87675)
+    //     cy.get('input[id="homePhone"]').clear().type(1234567890)
+    //     cy.get('button[type="submit').click()
+    //     cy.url({timeout : 5000}).should('includes','/dashboard')
+    //     cy.contains('has')
+    // })
+
+    
+    
+
+    
+})
