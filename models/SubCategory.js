9292945ce5ddc086125731261b113f979072ca99
Nonovi <wangzx0724@gmail.com>
2020-03-31 01:59:25 -0500

    create Sub-Category schema and route api

---
+++ models/SubCategory.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema;
+
+const SubCategorySchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    category: {
+        type: ObjectId,
+        ref: 'Category',
+        required: true
+      },
+    updatedDate: {
+      type: Date,
+      default: Date.now
+    }
+  });
+  
+  module.exports = SubCategory = mongoose.model('subcategory', SubCategorySchema);
+  
