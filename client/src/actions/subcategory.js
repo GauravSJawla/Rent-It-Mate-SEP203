963cf030fa94038925506d55ccca2518dda12658
Nonovi <wangzx0724@gmail.com>
2020-04-12 12:54:12 -0500

    add subcategory action

---
+++ client/src/actions/subcategory.js
@@ -0,0 +1,60 @@
+import axios from "axios";
+import { ADD_SUBCATEGORY, GET_SUBCATEGORY, SUBCATEGORY_DELETED, SUBCATEGORY_ERROR } from "./types";
+
+//Create a Sub-Category
+
+export const createSubcategory = ({ name, categoryId }) => async (dispatch) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  const body = JSON.stringify({ name, categoryId });
+  try {
+    const res = await axios.post("/api/subcategory", body, config);
+    dispatch({
+      type: ADD_SUBCATEGORY,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: SUBCATEGORY_ERROR,
+      payload: { status: err },
+    });
+  }
+};
+
+//Get the Sub-Category
+
+export const getSubcategory = () => async (dispatch) => {
+  try {
+    const res = await axios.get("/api/subcategory");
+    dispatch({
+      type: GET_SUBCATEGORY,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: SUBCATEGORY_ERROR,
+      payload: { status: err },
+    });
+  }
+};
+
+// Delete a Sub-Category
+
+export const deleteSubcategory = () => async (dispatch) => {
+  if (window.confirm("Are you sure to delete this sub-category?")) {
+    try {
+      const res = await axios.delete("/api/subcategory");
+      dispatch({
+        type: SUBCATEGORY_DELETED,
+      });
+    } catch (err) {
+      dispatch({
+        type: SUBCATEGORY_ERROR,
+        payload: { status: err },
+      });
+    }
+  }
+};
