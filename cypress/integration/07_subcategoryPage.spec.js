28baca8ab4d361e62bc45a4d5b80bac2f2fa6421
Nonovi <wangzx0724@gmail.com>
2020-04-12 16:18:13 -0500

    add subcategory create cypress testing file

---
+++ cypress/integration/07_subcategoryPage.spec.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+describe("admin dashboard", () => {
+  it("should redirect to admin dashboard on admin login", () => {
+    cy.visit("http://localhost:3000/login");
+    cy.get('input[id="username"]').type("admin");
+    cy.get('input[id="password"]').type("admin");
+    cy.get('button[type = "submit"]').click();
+    cy.url({ timeout: 5000 }).should("includes", "/admin-Dashboard");
+    cy.contains("Subcategories").click();
+  });
+
+  it("should stay on the same page with input name", () => {
+    cy.get('input[id="name"]').type("TestSubcate");
+    cy.get('button[type="submit"]').click();
+  });
+
+  it("should stay on the same page with empty input name", () => {
+    cy.get('input[id="name"]').clear();
+    cy.get('div[id="categoryId"]').click().contains("Gardens").click();
+    cy.get('button[type="submit"]').click();
+  });
+
+  it("should create a new subcategory", () => {
+    cy.get('input[id="name"]').type("TestSubcate");
+    cy.get('button[type="submit"]').click();
+  });
+
+  it("should not create a duplicate subcategory", () => {
+    cy.get('button[type="submit"]').click();
+  });
+
+});
