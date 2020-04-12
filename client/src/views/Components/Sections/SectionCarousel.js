f9aeb65d590400fc906893d909f9cc4dffb00044
GauravSJawla <gauravsinghjawla@gmail.com>
2020-04-12 18:52:53 -0500

    final commit before deployment

--- client/src/views/Components/Sections/SectionCarousel.js
+++ client/src/views/Components/Sections/SectionCarousel.js
@@ -1,24 +1,24 @@
-import React from "react";
+import React from 'react';
 // react component for creating beautiful carousel
-import Carousel from "react-slick";
+import Carousel from 'react-slick';
 // @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 // core components
-import GridContainer from "components/Grid/GridContainer.js";
-import GridItem from "components/Grid/GridItem.js";
+import GridContainer from 'components/Grid/GridContainer.js';
+import GridItem from 'components/Grid/GridItem.js';
 
-import image1 from "assets/img/Furniture.jpg";
-import image2 from "assets/img/electronics.jpg";
-import image3 from "assets/img/tvshow.jpg";
+import image1 from 'assets/img/Furniture.jpg';
+import image2 from 'assets/img/electronics.jpg';
+import image3 from 'assets/img/tvshow.jpg';
 
-import styles from "assets/jss/material-kit-react/views/componentsSections/carouselStyle.js";
+import styles from 'assets/jss/material-kit-react/views/componentsSections/carouselStyle.js';
 
 const useStyles = makeStyles(styles);
 
 export default function SectionCarousel() {
   const classes = useStyles();
   const settings = {
-    width: "100%",
+    width: '100%',
     dots: true,
     infinite: true,
     speed: 500,
@@ -31,21 +31,17 @@ export default function SectionCarousel() {
       <div className={classes.container}>
         <GridContainer>
           <GridItem xs={12}>
-              <Carousel {...settings}>
-                <div>
-                  <img src={image1} alt="First slide" className="slick-image" />
-                </div>
-                <div>
-                  <img
-                    src={image2}
-                    alt="Second slide"
-                    className="slick-image"
-                  />
-                </div>
-                <div>
+            <Carousel {...settings}>
+              <div>
+                <img src={image1} alt='First slide' className='slick-image' />
+              </div>
+              <div>
+                <img src={image2} alt='Second slide' className='slick-image' />
+              </div>
+              {/* <div>
                   <img src={image3} alt="Third slide" className="slick-image" />
-                </div>
-              </Carousel>
+                </div> */}
+            </Carousel>
           </GridItem>
         </GridContainer>
       </div>
