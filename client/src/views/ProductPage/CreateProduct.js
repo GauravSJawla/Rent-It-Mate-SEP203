3d1e6ace6cb8e23e9d79d62243810948d6b9d697
SELT171920 <hemalatha-ramachandran@uiowa.edu>
2020-04-19 18:26:17 -0500

    Create product screen changes

--- client/src/views/ProductPage/CreateProduct.js
+++ client/src/views/ProductPage/CreateProduct.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 // @material-ui/core components
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 // core components
 import Footer from 'components/Footer/Footer.js';
 import GridContainer from 'components/Grid/GridContainer.js';
@@ -12,14 +12,26 @@ import GridItem from 'components/Grid/GridItem.js';
 import Button from 'components/CustomButtons/Button.js';
 import Card from 'components/Card/Card.js';
 import CardBody from 'components/Card/CardBody.js';
+import InputLabel from "@material-ui/core/InputLabel";
 import CardHeader from 'components/Card/CardHeader.js';
 import CardFooter from 'components/Card/CardFooter.js';
 import CustomInput from 'components/CustomInput/CustomInput.js';
 import styles from 'assets/jss/material-kit-react/views/loginPage.js';
-import Select from "react-select";
-import CustomDropdown from 'components/CustomDropdown/CustomDropdown.js';
+import Select from '@material-ui/core/Select';
+import TextField from '@material-ui/core/TextField';
+import Input from '@material-ui/core/Input';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import FormLabel from '@material-ui/core/FormLabel';
+// import Radio from '@material-ui/core/Radio';
+// import RadioGroup from '@material-ui/core/RadioGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputBase from '@material-ui/core/InputBase';
+//import Select from "react-select";
 import {getCategoryList} from '../../actions/category';
 import {getAllSubcategories} from '../../actions/subcategory';
+import "react-datepicker/dist/react-datepicker.css";
 
 import image from 'assets/img/bg7.jpg';
 
@@ -27,6 +39,41 @@ import image from 'assets/img/bg7.jpg';
 import { createProduct } from '../../actions/product';
 
 const useStyles = makeStyles(styles);
+
+const BootstrapInput = withStyles((theme) => ({
+  root: {
+    'label + &': {
+      marginTop: theme.spacing(3),
+    },
+  },
+  input: {
+    borderRadius: 4,
+    position: 'relative',
+    backgroundColor: theme.palette.background.paper,
+    border: '1px solid #ced4da',
+    fontSize: 16,
+    padding: '10px 26px 10px 12px',
+    transition: theme.transitions.create(['border-color', 'box-shadow']),
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:focus': {
+      borderRadius: 4,
+      borderColor: '#80bdff',
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+    },
+  },
+}))(InputBase);
 
 function CreateProduct({ createProduct, 
                             history,
@@ -39,6 +86,9 @@ function CreateProduct({ createProduct,
   setTimeout(function() {
     setCardAnimation('');
   }, 700);
+
+  const ITEM_HEIGHT = 48;
+ const ITEM_PADDING_TOP = 8;
 
   const [values, setValues] = useState({
     name: '',
@@ -54,12 +104,20 @@ function CreateProduct({ createProduct,
   });
   const classes = useStyles();
 
+  const [switchShipping, setSwitchShipping] = React.useState(false);
+
+  const toggleShipping = () => {
+    console.log('inside shipping')
+    setSwitchShipping((prev) => !prev);
+    console.log('after toggle', switchShipping)
+  };
+
   const {
     name,
     description,
     price,
     quantity,
-    shipping,
+    shipping = switchShipping,
     subcategory,
     photo,
     fromDate,
@@ -89,35 +147,23 @@ function CreateProduct({ createProduct,
     init();
   }, []);
 
-   const subCategoryList = [];
-
-   //onCategorySelectChange Event handler
-
-   const onCategorySelectChange = e => {
-     const categoryId = e.value;
-     subcategories.map(subcategory => {
-       if(subcategory.categoryId === categoryId){
-         subCategoryList.push(subcategory.name);
-       }
-     })
-     console.log('subcategoryList', subCategoryList);
-   };
-
   //OnChange event Handler
   const onChange = e => {
-    const name = e.target.id;
+    const name = e.target.id == undefined ? 'subcategory' : e.target.id;
     const value = name == 'photo' ? e.target.files[0] : e.target.value;
-    console.log(name + ' id and val ' + value);
+    console.log('name and value ;' , name + ' ' + value)
     formData.set(name, value);
     setValues({ ...values, [name]: value });
   };
-
+console.log('subcategories', subcategories)
   // OnSubmit Event Handler
   const onSubmit = e => {
     e.preventDefault();
-    console.log('formdata: ' + JSON.stringify(formData));
+    formData.set('shipping', switchShipping)
+    console.log('formdata: ' + formData);
     createProduct(formData, history);
   };
+
   return (
     <div>
       <div
@@ -135,12 +181,18 @@ function CreateProduct({ createProduct,
                 <form className={classes.form} onSubmit={e => onSubmit(e)}>
                   <CardHeader color='primary' className={classes.cardHeader}>
                     <h4>Create Your Product</h4>
-                    <p></p>
                     <div className={classes.socialLine}></div>
                   </CardHeader>
-                  <CardBody>
+                  <CardBody 
+                    style={{
+                      width: '100%',
+                      margin: 'auto',
+                      padding: '10px',
+                      minheight: '30vw'
+                }}
+                  >
                     <CustomInput
-                      labelText='Name...'
+                      labelText='Name *'
                       id='name'
                       formControlProps={{
                         fullWidth: true
@@ -153,7 +205,7 @@ function CreateProduct({ createProduct,
                       }}
                     />
                     <CustomInput
-                      labelText='Description...'
+                      labelText='Description'
                       id='description'
                       formControlProps={{
                         fullWidth: true
@@ -165,7 +217,7 @@ function CreateProduct({ createProduct,
                       }}
                     />
                     <CustomInput
-                      labelText='Price...'
+                      labelText='Price *'
                       id='price'
                       formControlProps={{
                         fullWidth: true
@@ -178,7 +230,7 @@ function CreateProduct({ createProduct,
                       }}
                     />
                     <CustomInput
-                      labelText='Quantity...'
+                      labelText='Quantity *'
                       id='quantity'
                       formControlProps={{
                         fullWidth: true
@@ -191,8 +243,8 @@ function CreateProduct({ createProduct,
                         autoComplete: 'off'
                       }}
                     />
-                    <CustomInput
-                      labelText='Shipping...'
+                    {/* <CustomInput
+                      labelText='Shipping *'
                       id='shipping'
                       formControlProps={{
                         fullWidth: true
@@ -204,40 +256,41 @@ function CreateProduct({ createProduct,
                         onChange: e => onChange(e),
                         autoComplete: 'off'
                       }}
-                    />
-                    {/* <CustomInput
-                      labelText='Category id...'
-                      id='category'
-                      formControlProps={{
-                        fullWidth: true
-                      }}
-                      inputProps={{
-                        type: 'text',
-                        value: category,
-                        required: true,
-                        onChange: e => onChange(e),
-                        autoComplete: 'off'
-                      }}
-                      // buttonText='Category'
-                      // dropdownList={[
-                      //   'Furniture',
-                      //   'Electronics',
-                      //   'Garden',
-                      //   'Kitchen',
-                      //   'Home'
-                      // ]}
                     /> */}
-                    <label>Category</label>
-                    <Select
-                    options={categoryList}
-                    id="categoryId"
-                    onChange={(e) => onCategorySelectChange(e)}
-                    />
-                      <Select
-                      options={subCategoryList}
-                      id="subcategory"
-                      onChange={(e) => onChange(e)}
+                    <FormControlLabel
+                        control = {<Switch checked = {switchShipping} onChange = {toggleShipping}/>}
+                        label = "Product can be shipped"
                       />
+                    <InputLabel id="demo-simple-select-outlined-label">Product category *</InputLabel>
+                    <Select
+                        labelId = "demo-simple-select-outlined-label"
+                        id = "subcategory"
+                        value={subcategory}
+                        renderValue={selected => {
+                          if (selected.length === 0) {
+                            return <em>Please select One</em>;
+                          }
+                          return (subcategories.map(subcat => (
+                            subcat._id === selected ? subcat.name : ''
+                          )))
+                        }}
+                        onChange= {(e) => onChange(e)}
+                        input = {<Input />}
+                        MenuProps = {{
+                          PaperProps:{
+                            style : {
+                              maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+                              width: 250,
+                            }
+                          }
+                        }}
+                        >
+                          {subcategories.map(subcategory => (
+                            <MenuItem key = {subcategory.name} value = {subcategory._id}>
+                              {subcategory.name}
+                            </MenuItem>
+                          ))}
+                    </Select>
                     <CustomInput
                       labelText='Photo...'
                       id='photo'
@@ -249,6 +302,28 @@ function CreateProduct({ createProduct,
                         onChange: e => onChange(e)
                       }}
                     />
+                    <TextField
+                          id="fromDate"
+                          label="Product Start Date *"
+                          type="date"
+                          className={classes.textField}
+                          onChange = {(e) => onChange(e)}
+                          InputLabelProps={{
+                                  shrink: true,
+                          }}
+                     />
+                     <br/>
+                     <br/>
+                     <TextField
+                          id="toDate"
+                          label="Product End Date *"
+                          type="date"
+                          className={classes.textField}
+                          onChange = {(e) => onChange(e)}
+                          InputLabelProps={{
+                                  shrink: true,
+                          }}
+                     />
                   </CardBody>
                   <CardFooter className={classes.cardFooter}>
                     <Button simple type='submit' color='primary' size='lg'>
