1b19151939c6d93e1d901d8674db96a60d930eac
GauravSJawla <gauravsinghjawla@gmail.com>
2020-04-25 02:51:30 -0500

    completed Advanced Search

--- client/src/views/SearchingPage/SearchFilterSection.js
+++ client/src/views/SearchingPage/SearchFilterSection.js
@@ -1,64 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 // @material-ui/core components
 import { makeStyles } from '@material-ui/core/styles';
 
 // @material-ui/icons
+import SearchIcon from '@material-ui/icons/Search';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 // core components
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
 import GridContainer from 'components/Grid/GridContainer.js';
 import GridItem from 'components/Grid/GridItem.js';
-import Slider from '@material-ui/core/Slider';
-import TextField from '@material-ui/core/TextField';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import CustomInput from 'components/CustomInput/CustomInput.js';
+import Button from 'components/CustomButtons/Button.js';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 
 import styles from 'assets/jss/material-kit-react/views/landingPageSections/productStyle.js';
 
 const useStyles = makeStyles(styles);
-const useAppBarStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
+const useExpandPanelStyles = makeStyles((theme) => ({
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+    color: 'white',
   },
 }));
 
-const marks = [
-  {
-    value: 0,
-    label: '0 miles',
-  },
-  {
-    value: 25,
-    label: '25 miles',
-  },
-  {
-    value: 50,
-    label: '50 miles',
-  },
-  {
-    value: 75,
-    label: '75 miles',
-  },
-  {
-    value: 100,
-    label: '100 miles',
-  },
-];
-
-const valuetext = (value) => {
-  return `${value}`;
-};
-
 export default function SearchFilterSection() {
   const classes = useStyles();
+  const classesExpandPanel = useExpandPanelStyles();
+  const [value, setValue] = useState('25 miles');
 
-  const classesAppBar = useAppBarStyles();
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
 
   return (
     <div className={classes.section}>
@@ -66,77 +46,91 @@ export default function SearchFilterSection() {
         <div>
           <GridContainer>
             <GridItem xs={12} sm={12} md={12}>
-              <AppBar
-                position='static'
-                style={{
-                  background: '#9C27B0',
-                }}
-              >
-                <Toolbar>
-                  <Typography variant='h6' className={classesAppBar.title}>
-                    Advanced Search
-                  </Typography>
-                </Toolbar>
-              </AppBar>
+              <div className={classesExpandPanel.root}>
+                <ExpansionPanel>
+                  <ExpansionPanelSummary
+                    expandIcon={
+                      <ExpandMoreIcon
+                        style={{
+                          color: 'white',
+                        }}
+                      />
+                    }
+                    style={{
+                      background: '#9C27B0',
+                    }}
+                  >
+                    <Typography className={classesExpandPanel.heading}>
+                      Advanced Search
+                    </Typography>
+                  </ExpansionPanelSummary>
+                  <ExpansionPanelDetails>
+                    <GridContainer>
+                      <GridItem xs={12} sm={12} md={6}>
+                        <FormControl component='fieldset'>
+                          <RadioGroup
+                            name='search-area'
+                            value={value}
+                            onChange={handleChange}
+                          >
+                            <FormControlLabel
+                              value='25 miles'
+                              control={
+                                <Radio
+                                  style={{
+                                    color: '#9C27B0',
+                                  }}
+                                />
+                              }
+                              label='25 miles'
+                            />
+                            <FormControlLabel
+                              value='50 miles'
+                              control={
+                                <Radio
+                                  style={{
+                                    color: '#9C27B0',
+                                  }}
+                                />
+                              }
+                              label='50 miles'
+                            />
+                            <FormControlLabel
+                              value='75 miles'
+                              control={
+                                <Radio
+                                  style={{
+                                    color: '#9C27B0',
+                                  }}
+                                />
+                              }
+                              label='75 miles'
+                            />
+                          </RadioGroup>
+                        </FormControl>
+                      </GridItem>
+                      <GridItem xs={12} sm={12} md={6}>
+                        <CustomInput labelText='Min. Price' id='float' />
+                      </GridItem>
+                      <GridItem xs={12} sm={12} md={6}>
+                        <CustomInput labelText='Zipcode' id='float' />
+                      </GridItem>
+                      <GridItem xs={12} sm={12} md={6}>
+                        <CustomInput labelText='Max. Price' id='float' />
+                      </GridItem>
+                      <GridItem xs={12} sm={12} md={4}></GridItem>
+                      <GridItem xs={12} sm={12} md={4}>
+                        <Button color='primary' round>
+                          <SearchIcon />
+                        </Button>
+                      </GridItem>
+                    </GridContainer>
+                  </ExpansionPanelDetails>
+                </ExpansionPanel>
+              </div>
             </GridItem>
           </GridContainer>
         </div>
-        <GridContainer>
-          <GridItem xs={12} sm={12} md={6}>
-            <h5 className={classes.title}>Search Area</h5>
-            <Slider
-              defaultValue={25}
-              getAriaValueText={valuetext}
-              aria-labelledby='discrete-slider-restrict'
-              step={null}
-              valueLabelDisplay='auto'
-              marks={marks}
-              style={{
-                color: '#9C27B0',
-              }}
-            />
-          </GridItem>
-          <GridItem xs={12} sm={12} md={6}>
-            <h5 className={classes.title}>Price</h5>
-            <form className={classes.root} noValidate autoComplete='off'>
-              <div>
-                <TextField
-                  id='minPrice'
-                  label='Min. Price'
-                  type='number'
-                  InputLabelProps={{
-                    shrink: true,
-                  }}
-                  InputProps={{
-                    inputProps: {
-                      min: 0,
-                    },
-                  }}
-                />
-              </div>
-            </form>
-          </GridItem>
-          <GridItem xs={12} sm={12} md={6}></GridItem>
-          <GridItem xs={12} sm={12} md={6}>
-            <form className={classes.root} noValidate autoComplete='off'>
-              <div>
-                <TextField
-                  id='maxPrice'
-                  label='Max. Price'
-                  type='number'
-                  InputLabelProps={{
-                    shrink: true,
-                  }}
-                  InputProps={{
-                    inputProps: {
-                      min: 0,
-                    },
-                  }}
-                />
-              </div>
-            </form>
-          </GridItem>
-        </GridContainer>
       </div>
     </div>
   );
